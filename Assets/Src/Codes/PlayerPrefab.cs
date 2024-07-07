76fbd8ccad4c575c310607bcd3cbbfa4dd247c9d
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-07-08 00:50:58 +0900

    Add files via upload

---
+++ Assets/Src/Codes/PlayerPrefab.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefab : MonoBehaviour
+{
+    public RuntimeAnimatorController[] animCon;
+    private Animator anim;
+    private SpriteRenderer spriter;
+    private Vector3 lastPosition;
+    private Vector3 currentPosition;
+    private uint playerId;
+    TextMeshPro myText;
+
+    private float FLIP_THRESHOLD = 0.1f;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriter = GetComponent<SpriteRenderer>();
+        myText = GetComponentInChildren<TextMeshPro>();
+    }
+
+    public void Init(uint playerId, string id)
+    {
+        anim.runtimeAnimatorController = animCon[playerId];
+        lastPosition = Vector3.zero;
+        currentPosition = Vector3.zero;
+        this.playerId = playerId;
+
+        if (id.Length > 5) {
+            myText.text = id[..5];
+        } else {
+            myText.text = id;
+        }
+        myText.GetComponent<MeshRenderer>().sortingOrder = 6;
+    }
+
+    void OnEnable()
+    {    
+        anim.runtimeAnimatorController = animCon[playerId];
+    }
+
+    // 서버로부터 위치 업데이트를 수신할 때 호출될 메서드
+    public void UpdatePosition(float x, float y)
+    {
+        // lastPosition = currentPosition;
+        // currentPosition = new Vector3(x, y);
+        // transform.position = currentPosition;
+        // ------
+        // lastPosition = currentPosition;
+        // currentPosition = Vector3.Lerp(lastPosition, new Vector3(x, y), 0.1f);
+        // transform.position = currentPosition;
+        // ------
+        lastPosition = currentPosition;
+        currentPosition = new Vector3(x, y);
+        Vector3 nextPos = Vector3.Lerp(lastPosition, currentPosition, 0.1f);
+        transform.position = nextPos;
+
+        UpdateAnimation();
+    }
+
+    void LateUpdate()
+    {
+        if (!GameManager.instance.isLive)
+        {
+            return;
+        }
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        // 현재 위치와 이전 위치를 비교하여 이동 벡터 계산
+        Vector2 inputVec = currentPosition - lastPosition;
+
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        // if (inputVec.x != 0) // && Mathf.Abs(inputVec.x) <= 0.5f)
+        if (Mathf.Abs(inputVec.x) > FLIP_THRESHOLD)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!GameManager.instance.isLive)
+        {
+            return;
+        }
+    }
+}
